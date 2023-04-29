ff5f1855ccb86cd780fdaa01b80534927c5f88e7
chiragjain009 <chirag.jain2018@vitstudent.ac.in>
2023-04-30 02:37:16 +0530

    Create add user functionality

---
+++ src/users/AddUser.js
@@ -0,0 +1,126 @@
+import axios from 'axios';
+import React from 'react'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+
+export default function AddUser() {
+
+    let navigate=useNavigate();
+
+    const [user, setUser] = useState({
+        name: "",
+        username: "",
+        rank: 0,
+        address: "",
+        city: "",
+        phoneNo: ""
+    });
+
+    const { name, username, rank, address, city, phoneNo } = user
+
+    const onInputChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value });
+    };
+
+    const onSubmit =async (e) => {  
+        e.preventDefault();
+        // console.log(user);
+        await axios.post("http://localhost:8080/user",user); 
+        navigate("/");
+
+
+    };
+
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
+                    <h2 className='text-center m-4'>Register User</h2>
+
+                    <form onSubmit={(e) => onSubmit(e)}>
+                        <div className='mb-3'>
+                            <label htmlFor="name" className='form-label'>
+                                name
+                            </label>
+                            <input type={'text'}
+                                className='form-control'
+                                placeholder='Enter your name'
+                                name="name"
+                                value={name}
+                                onChange={(e) => onInputChange(e)}
+                            />
+                        </div>
+
+                        <div className='mb-3'>
+                            <label htmlFor="username" className='form-label'>
+                                username
+                            </label>
+                            <input type={'text'}
+                                className='form-control'
+                                placeholder='Enter your username'
+                                name="username"
+                                value={username}
+                                onChange={(e) => onInputChange(e)} />
+                        </div>
+
+                        <div className='mb-3'>
+                            <label htmlFor="address" className='form-label'>
+                                rank
+                            </label>
+                            <input type={'number'}
+                                className='form-control'
+                                placeholder='Enter your rank'
+                                name="rank"
+                                value={rank}
+                                onChange={(e) => onInputChange(e)} />
+                        </div>
+
+                        <div className='mb-3'>
+                            <label htmlFor="address" className='form-label'>
+                                address
+                            </label>
+                            <input type={'text'}
+                                className='form-control'
+                                placeholder='Enter your address'
+                                name="address"
+                                value={address}
+                                onChange={(e) => onInputChange(e)} />
+                        </div>
+
+                        <div className='mb-3'>
+                            <label htmlFor="city" className='form-label'>
+                                city
+                            </label>
+                            <input type={'text'}
+                                className='form-control'
+                                placeholder='Enter your city'
+                                name="city"
+                                value={city}
+                                onChange={(e) => onInputChange(e)} />
+                        </div>
+
+                        <div className='mb-3'>
+                            <label htmlFor="phoneNo" className='form-label'>
+                                phoneNo
+                            </label>
+                            <input type={'text'}
+                                className='form-control'
+                                placeholder='Enter your phoneNo'
+                                name="phoneNo"
+                                value={phoneNo}
+                                onChange={(e) => onInputChange(e)} />
+                        </div>
+
+                        <button type='submit' className='btn btn-outline-primary'>
+                            Submit
+                        </button>
+
+                        <Link type='submit' className='btn btn-outline-danger mx-2' to="/">
+                            Cancel
+                        </Link>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
